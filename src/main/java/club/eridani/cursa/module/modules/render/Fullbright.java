99782736c00956817885bc4b54ac59e8952c4e7c
Natsumikan2782 <mikan202182@outlook.jp>
2021-10-07 23:38:45 +0900

    Added Fullbright

---
+++ src/main/java/club/eridani/cursa/module/modules/render/Fullbright.java
@@ -0,0 +1,30 @@
+package club.eridani.cursa.module.modules.render;
+
+import club.eridani.cursa.common.annotations.Module;
+import club.eridani.cursa.common.annotations.Parallel;
+import club.eridani.cursa.module.Category;
+import club.eridani.cursa.module.ModuleBase;
+import club.eridani.cursa.setting.Setting;
+
+import java.util.Set;
+
+@Parallel
+@Module(name = "Fullbright", category = Category.RENDER)
+public class Fullbright extends ModuleBase {
+
+    Setting<Integer> gamma = setting("gamma",200,10, 500);
+
+    @Override
+    public void onTick (){
+        mc.gameSettings.gammaSetting = gamma.getValue();
+
+    }
+
+
+    @Override
+    public void onDisable() {
+        mc.gameSettings.gammaSetting = gamma.getValue();
+
+    }
+}
+
