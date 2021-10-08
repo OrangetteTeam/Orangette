f95bc1633cc09030942ce011cdc617655a349a41
g_user <yu.sute22@gmail.com>
2021-10-08 18:36:03 +0900

    Create XCarry.java

---
+++ src/main/java/club/eridani/cursa/module/modules/misc/XCarry.java
@@ -0,0 +1,23 @@
+package club.eridani.cursa.module.modules.misc;
+
+import club.eridani.cursa.common.annotations.Module;
+import club.eridani.cursa.common.annotations.Parallel;
+import club.eridani.cursa.event.events.network.PacketEvent;
+import club.eridani.cursa.module.Category;
+import club.eridani.cursa.module.ModuleBase;
+import net.minecraft.network.play.client.CPacketCloseWindow;
+
+@Parallel
+@Module(name = "XCarry", category = Category.MISC)
+public class XCarry extends ModuleBase {
+
+    @Override
+    public void onPacketSend(PacketEvent.Send event) {
+        if (event.getPacket() instanceof CPacketCloseWindow) {
+            if (((CPacketCloseWindow) event.getPacket()).windowId == mc.player.inventoryContainer.windowId) {
+                event.cancel();
+            }
+        }
+    }
+
+}
