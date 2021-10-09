a0f9ad19a15e010f8340af8b7caec1bcf8c3b9e9
g_user <yu.sute22@gmail.com>
2021-10-09 19:37:44 +0900

    Update KillAura.java

--- src/main/java/club/eridani/cursa/module/modules/combat/KillAura.java
+++ src/main/java/club/eridani/cursa/module/modules/combat/KillAura.java
@@ -4,6 +4,7 @@ import club.eridani.cursa.common.annotations.Module;
 import club.eridani.cursa.common.annotations.Parallel;
 import club.eridani.cursa.module.Category;
 import club.eridani.cursa.module.ModuleBase;
+import club.eridani.cursa.setting.Setting;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.network.play.client.CPacketUseEntity;
 import net.minecraft.util.EnumHand;
@@ -12,12 +13,13 @@ import net.minecraft.util.EnumHand;
 @Module(name = "KillAura", category = Category.COMBAT)
 public class KillAura extends ModuleBase {
 
+    public Setting<Double> range = setting("Range", 6.0, 0.1, 10);
     @Override
     public void onTick() {
         EntityLivingBase target = (EntityLivingBase) mc.world.loadedEntityList.stream().filter((entity) ->
                 entity instanceof EntityLivingBase &&
                 entity.isEntityAlive() &&
-                entity.getDistance(mc.player) <= 6 &&
+                entity.getDistance(mc.player) <= range.getValue() &&
                 entity != mc.player
                 ).findFirst().orElse(null);
 
