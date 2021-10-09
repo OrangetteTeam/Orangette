48ff7ad270a839e1a23b7f4d0ce176f20029046d
g_user <yu.sute22@gmail.com>
2021-10-09 19:32:46 +0900

    Update KillAura.java

--- src/main/java/club/eridani/cursa/module/modules/combat/KillAura.java
+++ src/main/java/club/eridani/cursa/module/modules/combat/KillAura.java
@@ -13,22 +13,21 @@ import net.minecraft.util.EnumHand;
 public class KillAura extends ModuleBase {
 
     @Override
-    public void onTick(){
+    public void onTick() {
+        EntityLivingBase target = (EntityLivingBase) mc.world.loadedEntityList.stream().filter((entity) ->
+                entity instanceof EntityLivingBase &&
+                entity.isEntityAlive() &&
+                entity.getDistance(mc.player) <= 6 &&
+                entity != mc.player
+                ).findFirst().orElse(null);
 
-        EntityLivingBase  target = (EntityLivingBase) mc.world.loadedEntityList.stream().filter((entity)->entity instanceof EntityLivingBase &&entity.isEntityAlive()&&entity.getDistance(mc.player)<= 6 &&entity != mc.player).findFirst().orElse(null);
-
-        if(target != null){
-
-            if(mc.player.getCooledAttackStrength(0.0f)> 0.9f) {
-
+        if (target != null) {
+            if (mc.player.getCooledAttackStrength(0.0f)> 0.9f) {
                 mc.getConnection().sendPacket(new CPacketUseEntity(target));
                 mc.player.swingArm(EnumHand.MAIN_HAND);
                 mc.player.resetCooldown();
-
             }
-
         }
-
     }
 
 }
