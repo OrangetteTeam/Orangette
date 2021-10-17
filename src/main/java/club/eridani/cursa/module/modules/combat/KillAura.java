1f01b665ec56d3ad0e4004746d1b6bd9c14c1ee3
wevez <79446154+wevez@users.noreply.github.com>
2021-10-17 22:17:10 +0900

    KillAura Rotation

--- src/main/java/club/eridani/cursa/module/modules/combat/KillAura.java
+++ src/main/java/club/eridani/cursa/module/modules/combat/KillAura.java
@@ -1,10 +1,13 @@
 package club.eridani.cursa.module.modules.combat;
 
 import club.eridani.cursa.common.annotations.Module;
+import club.eridani.cursa.event.events.network.PacketEvent;
 import club.eridani.cursa.module.Category;
 import club.eridani.cursa.module.ModuleBase;
 import club.eridani.cursa.setting.Setting;
+import club.eridani.cursa.utils.RotationUtil;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.network.play.client.CPacketPlayer;
 import net.minecraft.network.play.client.CPacketUseEntity;
 import net.minecraft.util.EnumHand;
 
@@ -12,6 +15,8 @@ import net.minecraft.util.EnumHand;
 public class KillAura extends ModuleBase {
 
     public Setting<Double> range = setting("Range", 6.0, 0.1, 10);
+
+    private float[] rotations;
 
     @Override
     public void onTick() {
@@ -23,6 +28,7 @@ public class KillAura extends ModuleBase {
         ).findFirst().orElse(null);
 
         if (target != null) {
+            rotations = RotationUtil.getRotationsAAC(target);
             if (mc.player.getCooledAttackStrength(0.0f) > 0.9f) {
                 mc.getConnection().sendPacket(new CPacketUseEntity(target));
                 mc.player.swingArm(EnumHand.MAIN_HAND);
@@ -31,4 +37,15 @@ public class KillAura extends ModuleBase {
         }
     }
 
+    @Override
+    public void onPacketSend(PacketEvent.Send event) {
+        if(rotations != null && event.getPacket() instanceof CPacketPlayer) {
+            CPacketPlayer player = (CPacketPlayer) event.getPacket();
+            player.yaw = rotations[0];
+            player.pitch = rotations[1];
+            RotationUtil.lastRotations[0] = player.yaw;
+            RotationUtil.lastRotations[1] = player.pitch;
+            rotations = null;
+        }
+    }
 }
--- src/main/java/club/eridani/cursa/utils/RotationUtil.java
+++ src/main/java/club/eridani/cursa/utils/RotationUtil.java
@@ -1,6 +1,7 @@
 package club.eridani.cursa.utils;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
@@ -9,6 +10,7 @@ import net.minecraft.util.math.Vec3d;
 public class RotationUtil {
 
     public static Minecraft mc = Minecraft.getMinecraft();
+    public static float[] lastRotations = new float[2];
 
     public static float[] getRotations(Vec3d from, Vec3d to) {
         double difX = to.x - from.x;
@@ -16,6 +18,31 @@ public class RotationUtil {
         double difZ = to.z - from.z;
         double dist = MathHelper.sqrt((difX * difX + difZ * difZ));
         return new float[]{(float) MathHelper.wrapDegrees((Math.toDegrees(Math.atan2(difZ, difX)) - 90.0)), (float) MathHelper.wrapDegrees(Math.toDegrees(Math.atan2(difY, dist)))};
+    }
+
+    public static float[] getRotationsAAC(EntityLivingBase entity) {
+        double d1 = Math.sqrt(mc.player.getDistance(entity) * mc.player.getDistance(entity)) / 1.5D;
+        double d2 = entity.posX + (entity.posX - entity.prevPosX) * d1 * Math.random() - mc.player.posX +mc.player.motionX*Math.random();
+        double d3 = entity.posZ + (entity.posZ - entity.prevPosZ) * d1 * Math.random()  - mc.player.posZ +mc.player.motionZ*Math.random();
+        float oldPitch = 0;
+        for (double ySearch = 0.15D; ySearch < 1D; ySearch += 0.1D) {
+            float currentPitch = (float) -Math.toDegrees(Math.atan2((entity.boundingBox.minY + (entity.boundingBox.maxY - entity.boundingBox.minY) * ySearch)-mc.player.boundingBox.minY-mc.player.getEyeHeight(), Math.hypot(d2, d3)));
+            if (oldPitch == 0 || getRotationDifference(new float[] {0, currentPitch}) < getRotationDifference(new float[] {0, oldPitch})) {
+                oldPitch = currentPitch;
+            }
+        }
+        return new float[]{(float) Math.toDegrees(Math.atan2(d3, d2)) - 90.0F, oldPitch};
+    }
+
+    public static double getRotationDifference(float[] rotation) {
+        return getRotationDifference(rotation, lastRotations);
+    }
+    public static double getRotationDifference(float[] a, float[] b) {
+        return Math.hypot(getAngleDifference(a[0], b[0]), a[1] - b[1]);
+    }
+
+    private static float getAngleDifference(final float a, final float b) {
+        return ((((a - b) % 360F) + 540F) % 360F) - 180F;
     }
 
     public static float[] getRotationsBlock(BlockPos block, EnumFacing face, boolean Legit) {
