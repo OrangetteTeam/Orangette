e8a62f25a2bbdfd1d0f809505f40b95cd36a21c1
fastmac0111 <suikanayuta@na-cat.com>
2021-10-20 16:21:04 +0900

    Fix MusicPlayer

--- src/main/java/club/eridani/cursa/gui/sigma/Component.java
+++ src/main/java/club/eridani/cursa/gui/sigma/Component.java
@@ -2,57 +2,61 @@ package club.eridani.cursa.gui.sigma;
 
 import club.eridani.cursa.client.FontManager;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
+
+import java.io.IOException;
 
 public class Component {
     public Minecraft mc = Minecraft.getMinecraft();
-    public int x, y, width, height;
+    public FontRenderer fontRenderer = mc.fontRenderer;
+    public int x , y , width , height;
 
-    public int doRender(int x, int y, int mouseX, int mouseY) {
+    public int doRender(int x , int y, int mouseX , int mouseY){
         return 0;
     }
 
-    public void mouseClicked(int mouseX, int mouseY, int mouseButton) {
+    public void mouseClicked(int mouseX, int mouseY, int mouseButton){
     }
 
     public void mouseReleased(int mouseX, int mouseY, int state) {
     }
 
-    public void keyTyped(char typedChar, int key) {
+    public void keyTyped(char typedChar , int key) {
     }
 
-    public Boolean isMouseHovering(int mouseX, int mouseY) {
+    public Boolean isMouseHovering(int mouseX , int mouseY) {
         return x < mouseX && x + width > mouseX && y < mouseY && y + height > mouseY;
     }
 
-    public Boolean isMouseHovering(float mouseX, float mouseY, float cx, float cy, float cw, float ch) {
+    public Boolean isMouseHovering(float mouseX , float mouseY , float cx , float cy , float cw , float ch) {
         return cx < mouseX && cx + cw > mouseX && cy < mouseY && cy + ch > mouseY;
     }
 
-    public Boolean isMouseHovering(float mouseX, float mouseY, float cx, float cy, float cw, float ch, float diff) {
+    public Boolean isMouseHovering(float mouseX , float mouseY , float cx , float cy , float cw , float ch , float diff) {
         return cx - diff < mouseX && cx + cw + diff > mouseX && cy - diff < mouseY && cy + ch + diff > mouseY;
     }
 
-    public float getCenter(float a, float b, float c) {
+    public float getCenter(float a , float b , float c){
         return a + (b - c) / 2;
     }
 
-    public void drawString(String str, float x, float y, int color) {
-        FontManager.jelloFont.drawString(str, x, y, color);
+    public void drawString(String str , float x , float y , int color){
+        FontManager.jelloFont.drawString(str , x , y , color);
     }
 
-    public float getFontWidth(String str) {
+    public float getFontWidth(String str){
         return FontManager.jelloFont.getStringWidth(str);
     }
 
-    public float getFontHeight() {
+    public float getFontHeight(){
         return FontManager.jelloFont.getHeight();
     }
 
-    public String getName(String name) {
+    public String getName(String name){
         String r = "";
         boolean a = false;
-        for (char c : name.toCharArray()) {
-            if (!a) r += String.valueOf(c).toUpperCase();
+        for(char c : name.toCharArray()){
+            if(!a) r += String.valueOf(c).toUpperCase();
             else r += String.valueOf(c).toLowerCase();
             a = true;
         }
