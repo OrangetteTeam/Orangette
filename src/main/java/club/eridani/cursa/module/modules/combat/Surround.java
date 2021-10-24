54c46b10d619b1484cb97381cc21847ac7467216
Natsumikan2782 <mikan202182@outlook.jp>
2021-10-24 16:49:33 +0900

    Add Surround

--- src/main/java/club/eridani/cursa/module/modules/combat/Surround.java
+++ src/main/java/club/eridani/cursa/module/modules/combat/Surround.java
@@ -1,15 +1,19 @@
 package club.eridani.cursa.module.modules.combat;
 
 import club.eridani.cursa.common.annotations.Module;
+import club.eridani.cursa.concurrent.event.Listener;
+import club.eridani.cursa.event.events.client.KeyEvent;
 import club.eridani.cursa.module.Category;
 import club.eridani.cursa.module.ModuleBase;
 import club.eridani.cursa.setting.Setting;
 import club.eridani.cursa.utils.BlockInteractionHelper;
 import club.eridani.cursa.utils.EntityUtil;
 import club.eridani.cursa.utils.InventoryUtil;
+import com.google.common.eventbus.Subscribe;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Module(name = "Surround", category = Category.COMBAT)
 public class Surround extends ModuleBase {
@@ -21,7 +25,6 @@ public class Surround extends ModuleBase {
     @Override
     public void onTick() {
         {
-
             BlockPos[] block = new BlockPos[]{};
             block = new BlockPos[]{
                     new BlockPos(1, 0, 0),
@@ -38,11 +41,18 @@ public class Surround extends ModuleBase {
             };
 
             {
+
                 entity = mc.player;
                 if (entity == null) disable();
                 BlockPos pos = new BlockPos(entity);
 
+                mc.player.setPosition(pos.getX() + .5, pos.getY(), pos.getZ() + 0.5);
+
                 ob = InventoryUtil.getBlockHotbar(Blocks.OBSIDIAN);//黒曜石をHotbarから探す
+                if (ob == -1) {
+                    disable();
+                    return;
+                }
                 mc.player.inventory.currentItem = ob;//上で探したものに切り替え
                 mc.playerController.updateController();//操作を更新
                 for (BlockPos add : block) {
@@ -50,6 +60,13 @@ public class Surround extends ModuleBase {
                     BlockInteractionHelper.placeBlock(pos.add(add), false);//Block設置
                 }
             }
+        }
+    }
+
+    @Listener
+    public void onKeyEvent(KeyEvent event) {
+        if (event.getKey() == mc.gameSettings.keyBindJump.getKeyCode()) {
+            disable();
         }
     }
 }
