5d424befcbf1402a7c88727703c3209e8958162d
Natsumikan2782 <mikan202182@outlook.jp>
2021-10-24 10:49:48 +0900

    Add SelfTrap

---
+++ src/main/java/club/eridani/cursa/module/modules/combat/SelfTrap.java
@@ -0,0 +1,52 @@
+package club.eridani.cursa.module.modules.combat;
+
+import club.eridani.cursa.common.annotations.Module;
+import club.eridani.cursa.module.Category;
+import club.eridani.cursa.module.ModuleBase;
+import club.eridani.cursa.utils.BlockInteractionHelper;
+import club.eridani.cursa.utils.EntityUtil;
+import club.eridani.cursa.utils.InventoryUtil;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.math.BlockPos;
+
+@Module(name = "SelfTrap", category = Category.COMBAT)
+public class SelfTrap extends ModuleBase {
+
+    int ob;
+    EntityPlayer entity;
+
+
+    @Override
+    public void onTick() {
+        {
+
+            BlockPos[] block = new BlockPos[]{};
+            block = new BlockPos[]{
+                    new BlockPos(0, 0, 1),
+                    new BlockPos(0, 1, 1),
+                    new BlockPos(0, 2, 1),
+                    new BlockPos(0, 2, 0)
+            };
+
+            {
+                //ここに最初のblock置くコード書く
+            }
+
+            {
+                entity = mc.player;
+                if (entity == null)disable();
+                BlockPos pos = new BlockPos(entity);
+
+                ob = InventoryUtil.getBlockHotbar(Blocks.OBSIDIAN);//黒曜石をHotbarから探す
+                mc.player.inventory.currentItem = ob;//上で探したものに切り替え
+                mc.playerController.updateController();//操作を更新
+                for (BlockPos add : block) {
+
+                    BlockInteractionHelper.placeBlock(pos.add(add),false);//Block設置
+
+                }
+            }
+        }
+    }
+}
