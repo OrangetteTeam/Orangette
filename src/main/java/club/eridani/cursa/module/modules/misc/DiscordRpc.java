49fa8ee1e9df80afd70606d25f3b8548968188b2
Natsumikan2782 <mikan202182@outlook.jp>
2021-11-22 17:47:58 +0900

    RpcRonpa!

--- src/main/java/club/eridani/cursa/module/modules/misc/DiscordRpc.java
+++ src/main/java/club/eridani/cursa/module/modules/misc/DiscordRpc.java
@@ -1,19 +1,103 @@
 package club.eridani.cursa.module.modules.misc;
 
+import club.eridani.cursa.Cursa;
 import club.eridani.cursa.client.RPCManager;
 import club.eridani.cursa.common.annotations.Module;
 import club.eridani.cursa.module.Category;
 import club.eridani.cursa.module.ModuleBase;
+import club.eridani.cursa.setting.Setting;
+import club.minnced.discord.rpc.DiscordEventHandlers;
+import club.minnced.discord.rpc.DiscordRPC;
+import club.minnced.discord.rpc.DiscordRichPresence;
 
-@Module(name = "DiscordRPC", category = Category.MISC)
+import java.util.Objects;
+
+@Module(name = "DiscordRpc", category = Category.MISC)
 public class DiscordRpc extends ModuleBase {
-    @Override
-    public void onEnable() {
-        RPCManager.INSTANCE.enable();
+
+    public Setting<String> icon = setting("Icon","orange","orange","pekora");
+
+    public static RPCManager INSTANCE;
+    private Thread _thread = null;
+
+
+    public static void init() {
+        INSTANCE = new RPCManager();
     }
 
-    @Override
+    public void onEnable() {
+        if (icon.getValue().equals("orange")) {
+            DiscordRPC lib = DiscordRPC.INSTANCE;
+            String applicationId = "855296693708652564";
+            String steamId = "";
+            DiscordEventHandlers handlers = new DiscordEventHandlers();
+            lib.Discord_Initialize(applicationId, handlers, true, steamId);
+            DiscordRichPresence presence = new DiscordRichPresence();
+            presence.startTimestamp = System.currentTimeMillis() / 1000; // epoch second
+            lib.Discord_UpdatePresence(presence);
+            presence.largeImageText = "";
+            _thread = new Thread(() ->
+            {
+                while (!Thread.currentThread().isInterrupted()) {
+                    lib.Discord_RunCallbacks();
+                    presence.details = getDetails();
+                    presence.state = getState();
+                    presence.largeImageKey = "logo";
+                    presence.largeImageText = Cursa.MOD_VERSION;
+                    lib.Discord_UpdatePresence(presence);
+
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException ignored) {
+                    }
+                }
+            }, "RPC-Callback-Handler");
+
+            _thread.start();
+        }
+        if (icon.getValue().equals("pekora")){
+            DiscordRPC lib = DiscordRPC.INSTANCE;
+            String applicationId = "911525316013482024";
+            String steamId = "";
+            DiscordEventHandlers handlers = new DiscordEventHandlers();
+            lib.Discord_Initialize(applicationId, handlers, true, steamId);
+            DiscordRichPresence presence = new DiscordRichPresence();
+            presence.startTimestamp = System.currentTimeMillis() / 1000; // epoch second
+            lib.Discord_UpdatePresence(presence);
+            presence.largeImageText = "";
+            _thread = new Thread(() ->
+            {
+                while (!Thread.currentThread().isInterrupted()) {
+                    lib.Discord_RunCallbacks();
+                    presence.details = getDetails();
+                    presence.state = getState();
+                    presence.largeImageKey = "logo";
+                    presence.largeImageText = Cursa.MOD_VERSION;
+                    lib.Discord_UpdatePresence(presence);
+
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException ignored) {
+                    }
+                }
+            }, "RPC-Callback-Handler");
+
+            _thread.start();
+
+        }
+
+    }
+
     public void onDisable() {
-        RPCManager.INSTANCE.disable();
+        DiscordRPC.INSTANCE.Discord_Shutdown();
+        _thread = null;
+    }
+
+    public String getDetails() {
+        return Objects.isNull(mc.player) ? "MainMenu" : mc.player.getName();
+    }
+
+    public String getState() {
+        return Objects.isNull(mc.player) ? "" : Objects.isNull(mc.getCurrentServerData()) ? "SinglePlayer" : mc.getCurrentServerData().serverIP;
     }
 }
