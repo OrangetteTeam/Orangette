7a05b16e4ac5d99b09578e3bfe9d65c545da77ee
Natsumikan2782 <mikan202182@outlook.jp>
2021-11-24 21:40:43 +0900

    Fixed Collar change

--- src/main/java/club/eridani/cursa/module/modules/render/BurrowEsp.java
+++ src/main/java/club/eridani/cursa/module/modules/render/BurrowEsp.java
@@ -5,6 +5,7 @@ import club.eridani.cursa.common.annotations.Module;
 import club.eridani.cursa.event.events.render.RenderEvent;
 import club.eridani.cursa.module.Category;
 import club.eridani.cursa.module.ModuleBase;
+import club.eridani.cursa.setting.Setting;
 import club.eridani.cursa.utils.BlockUtil;
 import club.eridani.cursa.utils.ColorUtil;
 import club.eridani.cursa.utils.CursaTessellator;
@@ -17,13 +18,17 @@ import static org.lwjgl.opengl.GL11.GL_QUADS;
 
 @Module(name = "BurrowEsp", category = Category.RENDER)
 public class BurrowEsp extends ModuleBase {
-
+    public Setting<Integer> rL = setting("Red", 255, 0, 255);
+    public Setting<Integer> gL = setting("Green", 26, 0, 255);
+    public Setting<Integer> bL = setting("Blue", 42, 0, 255);
+    public Setting<Integer> aL = setting("Alpha", 42, 0, 255);
+    
     @Override
     public void onRenderWorld(RenderEvent event) {
 
         for (Entity entity : mc.world.playerEntities) {
             BlockPos pos = EntityUtil.getEntityPos(entity);
-            int color = ColorUtil.toRGBA(GUIManager.getRed(), GUIManager.getGreen(), GUIManager.getBlue(), 60);
+            int color = ColorUtil.toRGBA(rL.getValue(),gL.getValue(),bL.getValue(),aL.getValue());
             if (BlockUtil.getBlock(pos) != Blocks.AIR) {
                 CursaTessellator.prepare(GL_QUADS);
                 CursaTessellator.drawFullBox(pos, 1f, color);
